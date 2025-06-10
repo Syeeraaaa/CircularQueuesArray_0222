0381349f638501121ac55e7906afef892c626aee
Syeeraaaa <syeerasilvia@gmai.com>
2025-06-10 11:12:23 +0700

    menampilkan isi antrian dari FRONT ke REAR

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -71,5 +71,22 @@ class Queues{
             }
             cout << endl;
         }
+        else{
+            //jika FRONT_position > REAR_position iterasi front hingga akhir array
+            while (FRONT_position <= max -1)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            
+            FRONT_position = 0;
+
+            // iterasi dari awal array hingga REAR
+            while(FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
