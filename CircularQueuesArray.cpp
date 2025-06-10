410c2860f2b84d760885a38bd599b44ab5679dea
Syeeraaaa <syeerasilvia@gmai.com>
2025-06-10 10:57:50 +0700

    menampilkan pesan jika antrian kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -55,5 +55,10 @@ class Queues{
     void Display(){
         int FRONT_position = FRONT;
         int REAR_position = REAR;
+
+        //cek apakah antrian kosong
+        if (FRONT == -1){
+            cout << "Queue is empty\n";
+        }
     }
 };
